2db6966bba7a9552cefd0999fba7dfa7d6c6e0e2
EdG570 <cole570@hotmail.com>
2016-10-15 09:53:24 -0400

    Refactored google maps api error handling

--- app.js
+++ app.js
@@ -18,17 +18,18 @@ const address = encodeURIComponent(argv.a);
 
 axios({
   method: 'get',
-  url: `https://ma.googleapis.com/maps/api/geocode/json?address=${address}`,
+  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${address}`,
   json: true
 })
   .then((response) => {
     if (response.data.status === 'ZERO_RESULTS') {
       console.log('Unable to find that address');
       return;
+    } else if (response.data.status === 'OK') {
+      console.log(`Address: ${response.data.results[0].formatted_address}`);
+      console.log(`Latitude: ${response.data.results[0].geometry.location.lat}`);
+      console.log(`Longitude: ${response.data.results[0].geometry.location.lng}`);
     }
-    console.log(`Address: ${response.data.results[0].formatted_address}`);
-    console.log(`Latitude: ${response.data.results[0].geometry.location.lat}`);
-    console.log(`Longitude: ${response.data.results[0].geometry.location.lng}`);
   })
   .catch((error) => {
     console.log('Unable to connect to Google Servers');
